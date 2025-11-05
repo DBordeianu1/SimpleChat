e8e276691ecf016f72911a6b652796a4295fecaa
Daniela Bordeianu <danabordeianu55@gmail.com>
2025-11-05 16:19:43 -0500

    Changed the error message in ClientConsole.java to reflect the Testcase 2003's output

--- edu/seg2105/client/ui/ClientConsole.java
+++ edu/seg2105/client/ui/ClientConsole.java
@@ -60,8 +60,7 @@ public class ClientConsole implements ChatIF
     } 
     catch(IOException exception) 
     {
-      System.out.println("Error: Can't setup connection!"
-                + " Terminating client.");
+      System.out.println("ERROR - Can't setup connection! Terminating client.");
       System.exit(1);
     }
     
